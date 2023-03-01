d818daa46d6695bc95e3c17a9efbe2ae27439fd3
SURIYA_SPARK <124863259+Sparksuriya@users.noreply.github.com>
2023-03-01 15:42:30 +0530

    Update datefunction1.php

--- New folder/datefunction1.php
+++ New folder/datefunction1.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TODAY IS MY BIRTHDAY</title>
+    <title>DATE FUNCTION</title>
 </head>
 <body>
     <h1>Today Date</h1>
