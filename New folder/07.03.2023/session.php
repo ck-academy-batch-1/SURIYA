85bff53bf7d83fa5a893481ae8e25a02888f4dc5
SURIYA_SPARK <124863259+Sparksuriya@users.noreply.github.com>
2023-03-07 14:35:22 +0530

    Add files via upload

---
+++ New folder/07.03.2023/session.php
@@ -0,0 +1,18 @@
+<?php
+// Start the session
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Set session variables
+  
+$_SESSION["Rollnumber"] = "a16csdb53";
+$_SESSION["Name"] = "suriya";
+echo "Session variables are set.";
+?>
+
+</body>
+</html>
