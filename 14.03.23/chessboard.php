664a66fa12bc738819e4bf36e39a5714c8726947
suriya <suriyasakthivel611586@gmail.com>
2023-03-14 10:13:49 +0530

    Just two only completed

---
+++ 14.03.23/chessboard.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>CHESSBOARD</title>
+</head>
+<body>
+  <table width="400px" border="1px" cellspacing="0px">
+    <?php
+      $value = 0;
+  
+      for($col = 0; $col < 8; $col++) {
+        echo "<tr>";
+        $value = $col;
+  
+        for($row = 0; $row < 8; $row++) {
+          if($value%2 == 0) {
+            echo "<td height=40px width=20px bgcolor=black></td>";
+            $value++;
+          }
+          else {
+            echo "<td height=40px width=20px bgcolor=white></td>";
+            $value++;
+          }
+        }
+        echo "</tr>";
+      }
+    ?>
+  </table>
+</body>
+</html>
